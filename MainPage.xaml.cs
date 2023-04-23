45ef208ff90d6c116e4509c69fdb676c50262cb2
Kittyn XR <kittynvr@outlook.com>
2023-04-23 11:24:16 +0900

    all libs building

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,19 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using CoreOSC;
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Events;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Media.Imaging;
+using System.IO;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Documents;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
